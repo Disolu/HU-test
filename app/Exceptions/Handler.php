36991884b587fa483ebc8db88a433de9a7e92675
Juan Carlos <admin@altimea.com>
2015-12-04 16:22:45 -0500

    Configurando la pantalla de error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,42 +3,55 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
     protected $dontReport = [
         HttpException::class,
     ];
 
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $e
-     * @return void
-     */
     public function report(Exception $e)
     {
         return parent::report($e);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
-     * @return \Illuminate\Http\Response
-     */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+      if ($e instanceof ModelNotFoundException) {
+        $e = new NotFoundHttpException($e->getMessage(), $e);
+      }
+      if ($e instanceof NotFoundHttpException) {
+        return response(view('errors.404'), 404);
+      }
+      elseif($e instanceof NotFoundHttpException) {
+        return response(view('errors.400'), 400);
+      }
+      else if($e instanceof NotFoundHttpException) {
+        return response(view('errors.401'), 401);
+      }
+      else if($e instanceof NotFoundHttpException) {
+        return response(view('errors.403'), 403);
+      }
+      else if($e instanceof NotFoundHttpException) {
+        return response(view('errors.405'), 405);
+      }
+      else if($e instanceof NotFoundHttpException) {
+        return response(view('errors.408'), 408);
+      }
+      else if($e instanceof NotFoundHttpException) {
+        return response(view('errors.410'), 410);
+      }
+      else if($e instanceof NotFoundHttpException) {
+        return response(view('errors.500'), 500);
+      }
+      else if($e instanceof NotFoundHttpException) {
+        return response(view('errors.503'), 503);
+      }
+      return parent::render($request, $e);
     }
+
 }
